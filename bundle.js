f73829cef897789edfe5df9363473f0d85c9cd97
seanreed1111 <seanreed1111@gmail.com>
2016-10-11 19:53:30 -0400

    checks for className

--- bundle.js
+++ bundle.js
@@ -1,4 +1,8 @@
 var movePiece = function(event){
-console.log(event.target)
+  var location = event.target;
+  console.log(location);
+  if (location.className === "square") {
+    console.log("Can you spare a square?");
+  }
 }
 var board = document.getElementById("board").addEventListener("click", movePiece);
