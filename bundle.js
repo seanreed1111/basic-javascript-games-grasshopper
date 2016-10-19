6a295b6ffe4e71b03da952712cccf92f9932b006
seanreed1111 <seanreed1111@gmail.com>
2016-10-19 15:43:11 -0400

    adds functioning event listeners

--- bundle.js
+++ bundle.js
@@ -1,15 +1,59 @@
-function isNumber(testValue) {
-  return !isNaN(testValue)
+
+function load() {
+  initBoard();
+  setListeners();
+
+  var clickLocation = null;
+
+  function getClickLocation(event) {
+    clickLocation = event.target.dataset.location;
+    if (clickLocation != undefined) {
+      updateBoard();
+    }
+  }
+
+  function initBoard(){
+    // reset game Arrays
+    var currentBoardArray = ["L","L","L","L","","R","R","R","R"];
+    var winningBoardArray = ["R","R","R","R","","L","L","L","L"];
+    var startButton = document.getElementById("startButton");
+    var endButton = document.getElementById("endButton");
+    var board = document.getElementById("board");
+    //render(currentBoardArray);
+  }
+
+
+  function startGame(){
+    console.log("Starting new game");
+  }
+
+  function updateBoard(){
+    console.log("Click location is " +clickLocation);
+  }
+
+  function endGame() {
+    console.log("Bye Bye. Game Over");
+    console.log("Clearing Event Listeners");
+    clearListeners();
+    console.log("Event Listeners cleared");
+  }
+
+  function setListeners(){
+    board.addEventListener("click", getClickLocation);
+    startButton.addEventListener("click", startGame);
+    endButton.addEventListener("click", endGame);
+  }
+
+  function clearListeners(){
+    board.removeEventListener("click", getClickLocation);
+    startButton.removeEventListener("click", startGame);
+    endButton.removeEventListener("click", endGame);
+  }
 }
-function findLocation(event){
-  var location = event.target.dataset.location;
-  console.log("location "+location);
-  return location
-}
+
+load();
 
 
-function updateBoard(location, board){
 
-}
 
-var board = document.getElementById("board").addEventListener("click", findLocation);
+
