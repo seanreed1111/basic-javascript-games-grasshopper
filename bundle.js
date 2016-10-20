a0a58ad589d1d8168c5b47960811f2b094b0a053
seanreed1111 <seanreed1111@gmail.com>
2016-10-19 21:47:26 -0400

    adds movement on board to match movement in model

--- bundle.js
+++ bundle.js
@@ -1,37 +1,79 @@
 
 (function() {
-  initBoard();
-  setListeners();
+  var startButton = document.getElementById("startButton");
+  var endButton = document.getElementById("endButton");
+  var board = document.getElementById("board");
+  board.addEventListener("click", respondToBoardClick);
+  startButton.addEventListener("click", startGame);
+  endButton.addEventListener("click", endGame);
 
+ // starting and winning position
+  var currentBoardArray = ["L","L","L","L","","R","R","R","R"];
+  var winningBoardArray = ["R","R","R","R","","L","L","L","L"];
+
+
+// render the initial state
+// render()
   var clickLocation = null;
+  var blankIndex = currentBoardArray.indexOf("");
 
-  function getClickLocation(event) {
+  // board click event callback handler
+  function respondToBoardClick(event) {
     clickLocation = event.target.dataset.location;
-    if (clickLocation != undefined) {
-      updateBoard();
+
+    if (validMove()) {
+      currentBoardArray[blankIndex] = currentBoardArray[clickLocation];
+      currentBoardArray[clickLocation] = "";
+      console.log("current board is" +currentBoardArray);
+
+      updateUI(clickLocation, blankIndex);
+      blankIndex = clickLocation; //reset for future moves
     }
   }
 
-  function initBoard(){
-    // reset game Arrays
-    var currentBoardArray = ["L","L","L","L","","R","R","R","R"];
-    var winningBoardArray = ["R","R","R","R","","L","L","L","L"];
-    var startButton = document.getElementById("startButton");
-    var endButton = document.getElementById("endButton");
-    var board = document.getElementById("board");
-    //render(currentBoardArray);
+  function validMove(){
+
+    console.log("You have clicked location " +clickLocation);
+    console.log("that location has value " +currentBoardArray[clickLocation]);
+
+    console.log("The blank space is at index " +blankIndex);
+
+    var valid = (clickLocation != undefined 
+      && Math.abs(clickLocation - blankIndex) <= 2
+      && clickLocation - blankIndex != 0)
+
+    if(!valid) {
+      console.log("Sorry, that move is not allowed");
+    }
+
+    return valid;
+  }
+
+  function updateUI(initialPosition, finalPosition){
+    // move the element from the initial position to the final position
+    initialSelector = "[data-location=\"" + initialPosition.toString() + "\"]";    
+    finalSelector = "[data-location=\"" + finalPosition.toString() + "\"]";
+    console.log('initialPositionSelector is '+initialSelector);
+    console.log('finalPositionSelector is '+finalSelector);
+
+    initialPositionElement = document.querySelector(initialSelector);
+    finalPositionElement = document.querySelector(finalSelector);
+    console.log('initialPositionElement is '+initialPositionElement.innerHTML);    
+    console.log('finalPositionElement is '+finalPositionElement.innerHTML);
+
+    finalPositionElement.innerHTML = initialPositionElement.innerHTML;
+    initialPositionElement.innerHTML = "";
   }
 
 
+
+
+// startButton callback handler
   function startGame(){
     console.log("Starting new game.");
-    console.log("Initializing Board. Setting Event Listeners");
   }
 
-  function updateBoard(){
-    console.log("Click location is " +clickLocation);
-  }
-
+// startButton callback handler
   function endGame() {
     console.log("Bye Bye. Game Over");
     console.log("Clearing Event Listeners");
@@ -39,14 +81,9 @@
     console.log("Event Listeners cleared");
   }
 
-  function setListeners(){
-    board.addEventListener("click", getClickLocation);
-    startButton.addEventListener("click", startGame);
-    endButton.addEventListener("click", endGame);
-  }
 
   function clearListeners(){
-    board.removeEventListener("click", getClickLocation);
+    board.removeEventListener("click", respondToBoardClick);
     startButton.removeEventListener("click", startGame);
     endButton.removeEventListener("click", endGame);
   }
